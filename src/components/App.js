2e9f141a440d047f2e6bdfbdfe43289eaf345a7d
Ivy Nyambura <ikagam018@gmail.com>
2024-08-13 12:49:40 +0300

    removed application form

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import "../App.css";
-import ApplicationForm from "./ApplicationForm";
+
 import Footer from "./Footer";
 
 function App() {
   return (
     <div>
-      <ApplicationForm />
+      
   <Footer />
       
     </div>
