38243711ae95a2f12d06af1c4c20e66e1af7380e
Ivy Nyambura <ikagam018@gmail.com>
2024-08-15 17:19:53 +0300

    styled the apply now route

--- src/components/ApplicationForm.js
+++ src/components/ApplicationForm.js
@@ -65,7 +65,14 @@ const ApplicationForm = () => {
   };
 
   return (
+
     <div>
+      <h3 className="deadline">Application Deadline: September 30th, 2024 | Entrance Exams: October 15th, 2024</h3>
+    <div className="form">
+      <div className="apply-top">
+      <img id="form-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEg6bo3ya8C4GEhFORrFauCxWbGSdLPzXsIw&s" alt="school logo"></img>
+      <h2>Bayside international school</h2>
+      </div>
       <h1>Application Form</h1>
       <form onSubmit={handleSubmit}>
         <div>
@@ -119,19 +126,20 @@ const ApplicationForm = () => {
           />
         </div>
         <div>
-          <button type="submit">Submit</button>
+          <button type="submit">Apply Now</button>
         </div>
 
         {showLoading && (
           <img
-            className="loading"
-            src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExaTBseHhhaTkwNGczbHI0aGpkN3FvbG9iMzAwaW9ibmhpMmhuanpmZCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/IzigGVSs9fHjug20Mq/giphy.webp"
+            id="loading"
+            src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExZnhjcmt1cTQxNDJ0eGt2Z2JtMzdoczd5NTI1d2VkbnBpdDhqcmtldiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/uIJBFZoOaifHf52MER/200.webp"
             alt="loading screen"
           ></img>
         )}
 
         {isSuccessful && <ApplicationSuccessful />}
       </form>
+    </div>
     </div>
   );
 };
