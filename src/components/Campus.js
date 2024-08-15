a87f8df6c296736a49c49abd8bc2bb0749c29bfb
Joshua Nyambane <nyamsjosh@gmail.com>
2024-08-16 02:15:58 +0300

    Creation of campus page

--- src/components/Campus.js
+++ src/components/Campus.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
 import Alumnis from './Alumnis';
+import Infrastaructure from './Infrastracture';
 
 const Campus = () => {
   return (
     <div>
       <h1>Campus</h1>
-      <p>Welcome to the Campus page. Here you will find information about our campus facilities.</p>
-
+      <p>Welcome to the Campus page. Here you will have to get a view on some of our infrastructures and also get to see several views and feedbacks from our Alumnis. </p>
+      <hr/>
+      
       <div>
+        <Infrastaructure/>
+        <hr/>
         <Alumnis />
       </div>
     </div>
