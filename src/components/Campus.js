fa84f050af7b46da7b20a4a7d60752aa26805106
Ivy Nyambura <ikagam018@gmail.com>
2024-08-16 19:47:38 +0300

    fixed styling in main

--- src/components/Campus.js
+++ src/components/Campus.js
@@ -5,7 +5,7 @@ import Infrastaructure from './Infrastracture';
 
 const Campus = () => {
   return (
-    <div>
+    <div className='campus-div'>
       <h1>Campus</h1>
       <p>Welcome to the Campus page. Here you will have to get a view on some of our infrastructures and also get to see several views and feedbacks from our Alumnis. </p>
       <hr/>
