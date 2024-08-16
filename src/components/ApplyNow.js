3f0577e2877281a63548cea7903554504c6425b0
Ivy Nyambura <ikagam018@gmail.com>
2024-08-16 13:48:30 +0300

    added applynow component bacause it was not there

---
+++ src/components/ApplyNow.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ApplicationForm from './ApplicationForm';
+
+function ApplyNow() {
+  return (
+    <div>
+      <ApplicationForm />
+    </div>
+  )
+}
+
+export default ApplyNow;
+
