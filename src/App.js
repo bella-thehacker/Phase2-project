1b03d37a0aaa300c416456357b191b29c12d942e
Bob Oyier <boboyier92@gmail.com>
2024-08-16 19:17:06 +0300

    Fix missing package.json dependencies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-
-import React from 'react';import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Academics from './components/Academics';
@@ -10,30 +10,24 @@ import OurPartners from './components/OurPartners';
 import Footer from './components/Footer';
 import Campus from './components/Campus';
 
-
 const App = () => (
-  
-    <div>
-      <div>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/partners" element={<OurPartners/>} />
-        <Route path="/academics" element={<Academics />} />
-        <Route path="/co-curricular" element={<CoCurricular />} />
-        <Route path="/campus" element={<Campus />} />
-        <Route path="/apply-now" element={<ApplyNow />} />
-    
-        
-      </Routes>
-      </div>
-      
-      <div>
-      <Footer />
-      </div>
-    </div>
-   
-);
+	<div>
+		<div>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/partners" element={<OurPartners />} />
+				<Route path="/academics" element={<Academics />} />
+				<Route path="/co-curricular" element={<CoCurricular />} />
+				<Route path="/campus" element={<Campus />} />
+				<Route path="/apply-now" element={<ApplyNow />} />
+			</Routes>
+		</div>
 
+		<div>
+			<Footer />
+		</div>
+	</div>
+);
 
 export default App;
