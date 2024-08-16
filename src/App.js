fb541699949d316fc88ca6d9915ebcd88843bd7b
Ivy Nyambura <ikagam018@gmail.com>
2024-08-16 13:29:49 +0300

    fixed app.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,4 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-=======
 import React from 'react';import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
 import Navbar from './components/Navbar';
@@ -58,6 +34,6 @@ const App = () => (
     </div>
    
 );
->>>>>>> 615f742843c7222059ec747c3e24630a6247ea52
+
 
 export default App;
